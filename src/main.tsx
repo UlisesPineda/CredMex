f522db81dad30d5f9dad19f5b1fdde6996b35d94
Ulises Pineda <iosefpineda@outlook.com>
2024-09-25 19:00:06 -0600

    Correcciones en el base

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { AppRouter } from './router/AppRouter.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/CredMex/'>
+    <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   </React.StrictMode>,
